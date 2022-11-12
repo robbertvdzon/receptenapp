5df4aedefeff3314b450e18bf4a2d26da70d57c9
robbertvdzon <robbert.van.der.zon@ah.nl>
2022-11-12 10:28:07 +0100

    updates

--- lib/src/ui/receptenapp/recepts/ReceptEditInstructionsPage.dart
+++ lib/src/ui/receptenapp/recepts/ReceptEditInstructionsPage.dart
@@ -31,12 +31,6 @@ class _WidgetState extends State<ReceptEditInstructionsPage> {
   late EnrichedRecept _recept;
   late Recept _newRecept;
   var _recipesService = getIt<RecipesService>();
-  var _imageStorageService = getIt<ImageStorageService>();
-  var _eventBus = getIt<EventBus>();
-  var _appStateService = getIt<AppStateService>();
-  var _enricher = getIt<Enricher>();
-  StreamSubscription? _eventStreamSub;
-  Image? receptImage = null;
   final TextEditingController _textEditingController = TextEditingController();
 
   _WidgetState(EnrichedRecept recept) {
@@ -46,51 +40,14 @@ class _WidgetState extends State<ReceptEditInstructionsPage> {
 
   _saveRecept() {
     _newRecept.directions =  _textEditingController.text;
-
     _recipesService
         .saveRecept(_newRecept)
         .whenComplete(() => Navigator.pop(context));
   }
 
   @override
-  void initState() {
-    _eventStreamSub = _eventBus.on<ReceptChangedEvent>().listen((event) => _processEvent(event));
-  }
-
-  void _processEvent(ReceptChangedEvent event) {
-    if (event.recept.uuid == _recept.recept.uuid) {
-      setState(() {
-        Recept? updatedRecept = _appStateService.getRecipes().firstWhereOrNull((element) => element.uuid==_recept.recept.uuid);
-        if (updatedRecept != null) {
-          _recept = _enricher.enrichRecipe(updatedRecept);
-          receptImage = null;
-        }
-      });
-    }
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _eventStreamSub?.cancel();
-  }
-
-  @override
   Widget build(BuildContext context) {
     _textEditingController.text = _recept.recept.directions;
-
-    if (receptImage==null) {
-      final Future<Image> data = _imageStorageService.get300x300(_recept.recept.uuid);
-      data.then((value) =>
-          setState(() {
-            receptImage = value;
-          })
-      ).catchError((e) =>
-          print(e)
-      );
-    }
-
-    var img = receptImage != null ? receptImage : Image.asset("assets/images/transparant300x300.png", height: 300, width: 300, fit: BoxFit.cover);
 
     return Scaffold(
         appBar: AppBar(
@@ -100,16 +57,11 @@ class _WidgetState extends State<ReceptEditInstructionsPage> {
         child: Center(
             child: Column(
           children: <Widget>[
-            Container(
-              alignment: Alignment.topLeft,
-              padding: EdgeInsets.only(left: 0, bottom: 0, right: 20, top: 0),
-              child: img
-            ),
             TextField(
               controller: _textEditingController,
               keyboardType: TextInputType.multiline,
               minLines: 1,//Normal textInputField will be displayed
-              maxLines: 10,// when user presses enter it will adapt to it
+              maxLines: 20,// when user presses enter it will adapt to it
             ),
             ElevatedButton(
               child: Text('Save'),
