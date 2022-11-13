7187e95e0eab63ecb5104bc1380bd6ddf6e13ab3
robbertvdzon <robbert.van.der.zon@ah.nl>
2022-11-13 10:20:00 +0100

    updates

--- lib/src/ui/receptenapp/recepts/edit/ReceptEditTagsPage.dart
+++ lib/src/ui/receptenapp/recepts/edit/ReceptEditTagsPage.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import '../../../../GetItDependencies.dart';
 import '../../../../model/enriched/enrichedmodels.dart';
 import '../../../../model/recipes/v1/recept.dart';
+import '../../../../repositories/RecipesTagsRepository.dart';
 import '../../../../services/RecipesService.dart';
 
 class ReceptEditTagsPage extends StatefulWidget {
@@ -21,13 +22,26 @@ class _WidgetState extends State<ReceptEditTagsPage> {
   late EnrichedRecept _recept;
   late Recept _newRecept;
   var _recipesService = getIt<RecipesService>();
+  var _recipesTagsRepository = getIt<RecipesTagsRepository>();
+  var _tagsSet = Set<String>();
+  var _additionalTags= "";
 
   _WidgetState(EnrichedRecept recept) {
     this._recept = recept;
     this._newRecept = recept.recept;
+    _recept.tags.forEach((element) {
+      _tagsSet.add(element?.tag??"");
+    });
   }
 
   _saveRecept() {
+    _newRecept.tags = _tagsSet.toList();
+    _additionalTags.split(",").forEach((element) {
+      var tag =element.trim();
+      if (tag.isNotEmpty) {
+        _newRecept.tags.add(tag);
+      }
+    });
     _recipesService
         .saveRecept(_newRecept)
         .whenComplete(() => Navigator.pop(context));
@@ -43,17 +57,76 @@ class _WidgetState extends State<ReceptEditTagsPage> {
             child: Center(
                 child: Column(
                   children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(label: Text('Tags:')),
-                      initialValue: "${_recept.tags.map((e) => e?.tag).join(",")}",
+                    SizedBox(
+                      height: 650,
+                      width: 500,
+                      child: ListView(
+                        children: _recipesTagsRepository.getTags().tags.map((item) {
+                          return Container(
+                            child: Column(
+                              children: [
+                                Container(
+                                  constraints: BoxConstraints.expand(
+                                    height: 40.0,
+                                  ),
+                                  alignment: Alignment.center,
+                                  child: buildTag(item.tag??"unknown"),
+                                ),
+                              ],
+                            ),
+                            margin: EdgeInsets.all(0),
+                            padding: EdgeInsets.all(0),
+                          );
+                        }).toList(),
+                      ),
                     ),
+                    TextFormField(
+                      decoration: InputDecoration(label: Text('Additional new tags (comma separated):')),
+                      initialValue: _additionalTags,
+                      onChanged: (text) {
+                        _additionalTags = text;
+                      },
+                    ),
+
                     ElevatedButton(
-                      child: Text('SAVE'),
+                      child: Text('Save'),
                       onPressed: () {
                         _saveRecept();
                       },
                     ),
                   ],
                 ))));
+  }
+
+
+  Widget buildTag(String tag) {
+    return InkWell(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Checkbox(
+                  checkColor: Colors.white,
+                  value: _tagsSet.contains(tag),
+                  onChanged: (bool? value) {
+                    setState(() {
+                      if (value==true){
+                        _tagsSet.add(tag);
+                      }
+                      else{
+                        var res = _tagsSet.remove(tag);
+                      }
+                    });
+                  },
+                ),
+                Text(tag),
+              ],
+            )
+          ],
+        ));
   }
 }
