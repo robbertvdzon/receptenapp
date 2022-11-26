af4be9d8d03eb446a207a2ecd42270ef5403d382
robbertvdzon <robbert.van.der.zon@ah.nl>
2022-11-26 12:02:29 +0100

    updates

--- lib/src/ui/diaryapp/DiaryHomePage.dart
+++ lib/src/ui/diaryapp/DiaryHomePage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import '../../GetItDependencies.dart';
+import '../../model/diary/v1/diary.dart';
+import '../../model/recipes/v1/recept.dart';
+import '../../repositories/DiaryRepository.dart';
 import '../../repositories/UserRepository.dart';
 import '../ingredientsapp/ingredienttags/IngredientTagsPage.dart';
 
@@ -19,6 +22,7 @@ class DiaryHomePage extends StatefulWidget {
 
 class _DiaryHomePageState extends State<DiaryHomePage> {
   var userRepository = getIt<UserRepository>();
+  var _diaryRepository = getIt<DiaryRepository>(); // TODO: via service!
 
   _DiaryHomePageState() {
   }
@@ -42,16 +46,61 @@ class _DiaryHomePageState extends State<DiaryHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('Op deze pagina aan kunnen geven hoeveel gegeten'),
-            Text(''),
-            Text('- Op basis van de planner (bv 60% van pokebowl recept)'),
-            Text('- Losse dingen kunnen toevoegen (chips, nootjes etc)'),
-            Text('- De veelgebruikte tussendoortjes makkelijk kunnen zien (en toevoegen)'),
-          ],
+          children: buildChildren(),
         ),
       ),
     );
   }
+
+  List<Widget> buildChildren() {
+    var now = DateTime.now();
+    var today = DateTime(now.year, now.month, now.day);
+    int todayTS =  today.add(new Duration(days: 0)).millisecondsSinceEpoch;
+    int todayTSMin1 =  today.add(new Duration(days: -1)).millisecondsSinceEpoch;
+    int todayTSMin2 =  today.add(new Duration(days: -2)).millisecondsSinceEpoch;
+    int todayTSMin3 =  today.add(new Duration(days: -3)).millisecondsSinceEpoch;
+    int todayTSPlus1 =  today.add(new Duration(days: 1)).millisecondsSinceEpoch;
+    int todayTSPlus2 =  today.add(new Duration(days: 2)).millisecondsSinceEpoch;
+    int todayTSPlus3 =  today.add(new Duration(days: 3)).millisecondsSinceEpoch;
+    int todayTSPlus4 =  today.add(new Duration(days: 4)).millisecondsSinceEpoch;
+
+    var list = List<Widget>.empty(growable: true);
+    list.addAll(buildChildrenForDay(_diaryRepository.getDay(todayTSMin3)));
+    list.addAll(buildChildrenForDay(_diaryRepository.getDay(todayTSMin2)));
+    list.addAll(buildChildrenForDay(_diaryRepository.getDay(todayTSMin1)));
+    list.addAll(buildChildrenForDay(_diaryRepository.getDay(todayTS)));
+    list.addAll(buildChildrenForDay(_diaryRepository.getDay(todayTSPlus1)));
+    list.addAll(buildChildrenForDay(_diaryRepository.getDay(todayTSPlus2)));
+    list.addAll(buildChildrenForDay(_diaryRepository.getDay(todayTSPlus3)));
+    list.addAll(buildChildrenForDay(_diaryRepository.getDay(todayTSPlus4)));
+
+    return list;
+  }
+
+  List<Widget> buildChildrenForDay(DiaryDay? diaryDay) {
+    if (diaryDay==null) return List.empty();
+    List<Widget> result = List.empty(growable: true);
+    DateTime date = DateTime.fromMillisecondsSinceEpoch(diaryDay.dateAsTimestamp);
+    result.add(Text(''));
+    result.add(Text('Dag: ${date.toString()}:'));
+    result.add(Text('breakfast:'));
+    result.addAll(buildChildrenForPartOfDay(diaryDay.breakfast));
+    result.add(Text('morningSnack:'));
+    result.addAll(buildChildrenForPartOfDay(diaryDay.morningSnack));
+    result.add(Text('lunch:'));
+    result.addAll(buildChildrenForPartOfDay(diaryDay.lunch));
+    result.add(Text('afternoonSnack:'));
+    result.addAll(buildChildrenForPartOfDay(diaryDay.afternoonSnack));
+    result.add(Text('diner:'));
+    result.addAll(buildChildrenForPartOfDay(diaryDay.diner));
+    result.add(Text('eveningSnack:'));
+    result.addAll(buildChildrenForPartOfDay(diaryDay.eveningSnack));
+    return result;
+  }
+
+  List<Widget> buildChildrenForPartOfDay(List<Recept> meals) {
+    return  meals.map((e) => Text(e.name)).toList();
+  }
+
 }
 
